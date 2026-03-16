1fc4d97ac2ccd929599fc16ebf6d88a6af3613f2
JaroslawTusinski <jarektusinski@gmail.com>
2026-03-16 19:21:19 +0100

    feat: add orientation types and update dependencies
    - Add new orientation.ts file with text orientation types
    - Update package.json and package-lock.json for new dependencies
    - Modify src/index.ts to include orientation exports

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import Orientation from './orientation';
+
+export {
+    Orientation,
+    HorizontalOrientation,
+    VerticalOrientation,
+    TextOrientation,
+    MultiTextOrientation
+} from './orientation';
+
+export default {
+    Orientation
+}
